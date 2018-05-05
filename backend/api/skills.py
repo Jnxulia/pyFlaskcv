5ff95dd04069abaf6ad9a6ca98cf43eeea3110a2
Javier Alejandro Nava Niño <jnava@e-sign.cl>
2018-05-05 10:18:42 -0300

    config file

--- backend/api/skills.py
+++ backend/api/skills.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import g
+from flask import g , request
 from .utils import json_response
 from bson.json_util import dumps
 
---
+++ backend/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+setup(
+    install_requires=["pymongo==3.6.1","Flask==0.12.2","Flask-RESTful==0.3.6","requests ==2.18.4"],
+)
